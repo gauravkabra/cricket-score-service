4d35b8cb004a5ec4194b6077b5aadc80573638cb
gakabra <gaurav.kabra.it@gmail.com>
2019-06-20 23:55:39 +0530

    Project setup commit

---
+++ src/main/java/com/cricket/score/cricketscoreservice/CricketScoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.score.cricketscoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketScoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketScoreServiceApplication.class, args);
+	}
+
+}
