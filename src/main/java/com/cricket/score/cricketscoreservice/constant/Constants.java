67ef549492d66ac52eae978da858e89756b95920
gakabra <gaurav.kabra.it@gmail.com>
2019-06-21 02:20:37 +0530

    Commit for Swagger and get live score updates REST API

---
+++ src/main/java/com/cricket/score/cricketscoreservice/constant/Constants.java
@@ -0,0 +1,8 @@
+package com.cricket.score.cricketscoreservice.constant;
+
+public class Constants {
+
+	public static final String API_KEY = "QAtRxh1XVlONpcrSzmCh1QEePS33";
+	public static final String URL = "http://cricapi.com/api/cricketScore";
+
+}
---
+++ src/main/java/com/cricket/score/cricketscoreservice/controller/CricketLiveScoreController.java
@@ -0,0 +1,58 @@
+package com.cricket.score.cricketscoreservice.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.cricket.score.cricketscoreservice.bean.CricketLiveScore;
+import com.cricket.score.cricketscoreservice.bean.CricketScore;
+import com.cricket.score.cricketscoreservice.constant.Constants;
+
+@Controller
+public class CricketLiveScoreController {
+
+	private Logger logger = LoggerFactory.getLogger(CricketLiveScoreController.class);
+
+	@GetMapping("/getLiveScore/{unique_id}")
+	public HttpEntity<String> liveScore(@PathVariable String unique_id) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		logger.info("Created the Rest Template -->>");
+		
+		// Method #1
+		//final String uri = "http://cricapi.com/api/cricketScore/?unique_id="+unique_id+"&apikey="+Constants.API_KEY;
+		//String result = restTemplate.getForObject(uri, String.class);
+		//return result;
+		
+		// Method #2
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", "application/json");
+		
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(Constants.URL)
+				.queryParam("unique_id", unique_id)
+				.queryParam("apikey", Constants.API_KEY);
+		
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+		
+		// If you need to manipulate string use below get function to interact external REST
+		// String response1 = restTemplate.getForObject(builder.build().encode().toUri(), String.class);
+		// return response1;
+		
+		logger.info("Sending get request -->>");
+		HttpEntity<String> response = restTemplate.exchange(builder.toUriString(),
+					HttpMethod.GET, entity, String.class);
+		logger.info("Received response -->>", response.toString());
+		
+		return response;
+	}
+}
