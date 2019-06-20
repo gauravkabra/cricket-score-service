273a3921f098dcab439ca086c3ca3ca64790b01b
gakabra <gaurav.kabra.it@gmail.com>
2019-06-21 03:12:49 +0530

    Configuration for Creating war file

--- src/main/java/com/cricket/score/cricketscoreservice/CricketScoreServiceApplication.java
+++ src/main/java/com/cricket/score/cricketscoreservice/CricketScoreServiceApplication.java
@@ -2,12 +2,17 @@ package com.cricket.score.cricketscoreservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CricketScoreServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CricketScoreServiceApplication.class, args);
+	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CricketScoreServiceApplication.class);
 	}
 
 }
